c278df1ef231bf712ae9c55eebbbd9cc12c1a262
Dr-Positive <adejoriisrael@gmail.com>
2024-05-20 21:12:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/*
+html
+
+Twitter @i_amgoke: https://twitter.com/i_amgoke
+
+Github: https://www.github.com/Gokegold
+
+Date Created: [May 20, 2024]
+
+last modification:: [May 20, 2024]
+*/
+
 var settingsmenu = document.querySelector(".settings-menu");
 
 function settingsMenuToggle(){
